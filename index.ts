37170b52b90fa392e78afe7f3b7dadfd7ba5957f
Nojus0 <thealextgm@gmail.com>
2023-01-30 14:47:35 +0000

    Fix Formatting

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import * as bip39 from "@scure/bip39"
 import crypto from "crypto"
-import {wordlist} from "@scure/bip39/wordlists/english"
-;(async()=>{
+import { wordlist } from "@scure/bip39/wordlists/english"
+
+(async () => {
 
     const key = crypto.randomBytes(16)
     console.log(key)
